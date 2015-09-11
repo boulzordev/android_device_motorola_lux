478c9c00aba73525a859eb6730679ee151069145
Vachounet <vachounet@live.fr>
2015-09-11 17:52:10 +0200

    Typo fix

--- aosp_lux.mk
+++ aosp_lux.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-PRODUCT_RELEASE_NAME := osprey
+PRODUCT_RELEASE_NAME := lux
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
